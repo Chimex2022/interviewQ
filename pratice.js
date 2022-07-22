5d1b31135a1225ae522980fe17b5f981934fe4d5
Gabriel Anyaele <gabriel.anyaele@konga.local>
2022-07-22 22:06:43 +0100

    My project on interview question

---
+++ pratice.js
@@ -0,0 +1,28 @@
+
+const correctAnswer = ['B', 'B', 'B', 'B'];
+const form =document.querySelector('.quiz-form');
+const result =document.querySelector('.result')
+form.addEventListener('submit', e=>{
+    e.preventDefault();
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswer[index]){
+          score +=25;
+        }
+    });
+    scrollTo(0,0);
+    result.querySelector('span').textContent =`${score}%`;
+    result.classList.remove('d-none');
+    console.log(score);
+
+      let output = 0;
+      const timer =setInterval(() =>{
+        result.querySelector('span').textContent =`${output}%`;
+        if(output===score){
+          clearInterval(timer);
+        }else{
+          output++;
+        }
+      }, 10);
+    });
